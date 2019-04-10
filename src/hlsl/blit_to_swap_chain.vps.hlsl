88d09ed6e277c1be9dadf3e50d52d5232aef1941
yngccc <yngccc@gmail.com>
2019-04-09 21:08:13 -0400

    added aces film tone map

--- src/hlsl/blit_to_swap_chain.vps.hlsl
+++ src/hlsl/blit_to_swap_chain.vps.hlsl
@@ -25,10 +25,19 @@ float3 linear_to_srgb(float3 rgb) {
 	return max(1.055 * pow(rgb, 0.416666667) - 0.055, 0);
 }
 
+float3 aces_film_tone_map(float3 x) {
+	float a = 2.51f;
+	float b = 0.03f;
+	float c = 2.43f;
+	float d = 0.59f;
+	float e = 0.14f;
+	return saturate((x* (a * x + b)) / (x * (c * x + d) + e));
+}
+
 ps_output pixel_shader(vs_output vs_output) {
 	ps_output output;
 	output.color = src_texture.Sample(texture_sampler, vs_output.texcoord);
-//	output.color.rgb = output.color.rgb / (output.color.rgb + float3(1, 1, 1));
+	output.color.rgb = aces_film_tone_map(output.color.rgb);
 	float dither = dither_texture.Sample(dither_sampler, vs_output.position.xy / 8.0) / 32.0 - (1.0 / 128.0);
 	output.color.rgb += float3(dither, dither, dither);
 	output.color.rgb = linear_to_srgb(output.color.rgb);
